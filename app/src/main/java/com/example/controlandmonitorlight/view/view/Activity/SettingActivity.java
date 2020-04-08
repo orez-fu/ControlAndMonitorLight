9f46eac98ec967f8e291b9b3ecb8c5dd3ca2a2e4
vuongit98 <vuongnguyen1300@gmail.com>
2020-04-09 00:14:05 +0700

    update button logout

--- app/src/main/java/com/example/controlandmonitorlight/view/view/Activity/SettingActivity.java
+++ app/src/main/java/com/example/controlandmonitorlight/view/view/Activity/SettingActivity.java
@@ -3,14 +3,26 @@ package com.example.controlandmonitorlight.view.view.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.controlandmonitorlight.R;
 
 public class SettingActivity extends AppCompatActivity {
 
+    Button btLogout ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        btLogout = findViewById(R.id.button) ;
+        btLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
